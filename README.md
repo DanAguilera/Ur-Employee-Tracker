c9dbf28ad22316daf9f71112ebedfe1b9ec7df8f
Daniel Aguilera <dan.aguilera97@gmail.com>
2023-01-16 14:06:23 -0700

    updated readMe

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Ur-Employee-Tracker
+
+## video link
+https://watch.screencastify.com/v/nteDK7lw6S5XWqMoKQHz
+
+The purpose of this project was to use the SQL database to create and store and update employee info. With this youre able to view employee, dept and roles; as well as add employees departments and roles. I was able to get closely familiar with SQL and understand concepts and terms like seeds, schemas and how to set up inquirer and be able to connect to a port by properly setting up your main index,js file. 
--- index.js
+++ index.js
@@ -36,7 +36,6 @@ function firstPrompt() {
         "View All Roles",
         "View All Departments",
         "Add Employee",
-        "Remove Employees",
         "Update Employee Role",
         "Add Role",
         "Add Department",
@@ -98,12 +97,12 @@ function viewAllEmployee() {
 }
 
 function viewAllRoles() {
-  console.log("Viewing employees\n");
+  console.log("Viewing Roles\n");
 
   var query =
     `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
-  LEFT employee e
-  FROM JOIN role r
+  FROM employee e
+  LEFT JOIN role r
 	ON e.role_id = r.id
   LEFT JOIN department d
   ON d.id = r.department_id
@@ -121,14 +120,14 @@ function viewAllRoles() {
 }
 
 function viewAllDepartments() {
-  console.log("Viewing employees\n");
+  console.log("Viewing Department\n");
 
   var query =
     `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
-  LEFT employee e
+  FROM employee e
   LEFT JOIN role r
 	ON e.role_id = r.id
-  FROM JOIN department d
+  LEFT JOIN department d
   ON d.id = r.department_id
   LEFT JOIN employee m
 	ON m.id = e.manager_id`
